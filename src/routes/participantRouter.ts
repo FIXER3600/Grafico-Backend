f342536e7c91b79e9c84ea6863faa1e836cd4ad1
FIXER3600 <guilhermecomercial51@gmail.com>
2022-08-10 00:33:20 -0300

    Deploy

---
+++ src/routes/participantRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { ParticipantController } from "../controller/ParticipantController";
+
+export const participantRouter=express.Router()
+const participantController=new ParticipantController()
+
+participantRouter.post("/create",participantController.insert)
+participantRouter.get('/all',participantController.getAll)
